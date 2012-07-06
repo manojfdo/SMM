fa7af458790257161372291ab122488a0e511b94
Francis Manoj Fernando <manojfdo@ymail.com>
2012-07-06 13:29:25 +0530

    template by mana

---
+++ Templates/Main.dwt.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body style="margin:0; padding:0;" onload="MM_preloadImages('../img/home_over.png','../img/classifieds_over.png','../img/MyAccount_over.png')">
+<table width="100%" border="0" align="center" cellpadding="1" cellspacing="0">
+  <tr>  </tr>
+</table>
+<span style="text-align: center"></span>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td colspan="2" align="center"><img src="../img/top_border.png" width="100%" height="14" /></td>
+  </tr>
+  <tr>
+    <td width="57%" rowspan="2" ><table width="400" border="0" align="left" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><img src="../img/sell_my_mobile_logo.png" width="379" height="80" /></td>
+      </tr>
+    </table></td>
+    <td width="43%" ><table width="400" border="0" align="right" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="100"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('home_navi','','../img/home_over.png',1)"><img src="../img/home_normal.png" name="home_navi" width="100" height="30" border="0" id="home_navi" /></a></td>
+        <td width="155"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('class_navi','','../img/classifieds_over.png',1)"><img src="../img/classifieds_normal.png" name="class_navi" width="174" height="32" border="0" id="class_navi" /></a></td>
+        <td width="145"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('myacc_navi','','../img/MyAccount_over.png',1)"><img src="../img/myaccount_normal.png" name="myacc_navi" width="173" height="32" border="0" id="myacc_navi" /></a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td >&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2"><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="50%">&nbsp;</td>
+        <td width="800"><!-- TemplateBeginEditable name="Body" -->Body have to be not more than 800px<!-- TemplateEndEditable --></td>
+        <td width="50%">&nbsp;</td>
+      </tr>
+      <tr>
+        <td bgcolor="#CCCCCC"><img src="../img/footer_line.png" alt="" width="100%" height="5" hspace="0" vspace="0" /></td>
+        <td bgcolor="#CCCCCC"  align="center"><img src="../img/footer_line.png" width="800" height="5" hspace="0" vspace="0" /></td>
+        <td bgcolor="#CCCCCC"><img src="../img/footer_line.png" alt="" width="100%" height="5" hspace="0" vspace="0" /></td>
+      </tr>
+      <tr>
+        <td bgcolor="#CCCCCC">&nbsp;</td>
+        <td width="800" bgcolor="#CCCCCC"  align="center"><p style="font-weight:bold">Sell My Mobile<br />
+          National Institute Of Busniness Management - Higher Diploma In Computer Based Information Systems 11.1</p></td>
+        <td bgcolor="#CCCCCC">&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
