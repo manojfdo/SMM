577caeb516971fbdc8042cf3f8d88df77d39ffc0
Francis Manoj Fernando <manojfdo@ymail.com>
2012-07-07 12:49:18 +0530

    config
    create a user named 'smm'
    password 'smm123'
    database 'smym'
    get the dump file for the database

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'smm';
+$db['default']['password'] = 'smm123';
+$db['default']['database'] = 'smym';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
